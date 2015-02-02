d24ceb7e63d10e21f55279b87a73d8fb05a087b5
Calvin <ashmore@gmail.com>
2015-02-02 08:54:45 -0800

    in progress monkeying around

---
+++ src/main/java/com/icosilune/fn/AbstractFn.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.icosilune.fn;
+
+import java.util.Map;
+
+/**
+ *
+ * @author ashmore
+ */
+public abstract class AbstractFn {
+
+  public abstract Map<String, FnType> getInputTypes();
+  public abstract Map<String, FnType> getOutputTypes();
+
+  /**
+   * Types that use implement {@link Fn} and implement {@link AbstractFn} should
+   * implement {@code evaluate(arguments...)}, which returns the single output type.
+   * if there is a single output type. If there are multiple output types, then {@code evaluate}
+   * should return a {@code Map<String, Object>}
+   *
+   *
+   * ******* what if the output type is a map?
+   * ** Also, how do we know the names ahead of time?
+   */
+  public abstract Map<String, FnType> evaluateWrapper(
+          EvaluationContext context, Map<String, Object> args);
+}
---
+++ src/main/java/com/icosilune/fn/FnType.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.icosilune.fn;
+
+/**
+ *
+ * @author ashmore
+ */
+public class FnType<T> {
+
+  // T is class
+  // ****** there is java reflect Type which may be better than this
+  // or, we could just keep a reference to that, and use additional stuff
+
+  // denotes input/output type, maybe has case for error conditions, defaults, etc
+  // class + maybe generics???
+}
